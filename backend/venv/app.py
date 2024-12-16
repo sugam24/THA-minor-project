3d5e6155adba0f6c3ffefce7413a79f8eca5f142
praniil <pparajuli15@gmail.com>
2024-12-16 21:43:59 +0545

    getting the user data from the database

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -1,24 +1,21 @@
 import flask
 import json
-from flask import jsonify
-from flask import request
+from flask import jsonify, request
 from flask_cors import CORS
 import database_con as db
 
-#database connection
-print("connected to the database")
-
+# Database connection
+print("Connected to the database")
 cursor = db.connection.cursor()
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
-@app.route('/api/post_data', methods= ['POST'])
+@app.route('/api/post_data', methods=['POST'])
 def post_data():
     data = request.get_json()
 
-    #extract information
     first_name = data.get('firstname')
     last_name = data.get('lastname')
     email = data.get('email')
@@ -26,30 +23,52 @@ def post_data():
     confirm_password = data.get('confirmPassword')
     print(first_name, last_name, email, password, confirm_password)
 
+    # Create table if not exists
     create_register_table = '''
-        CREATE TABLE if not exists Register(
+        CREATE TABLE IF NOT EXISTS Register(
             id SERIAL PRIMARY KEY,
-            first_name varchar(50),
-            last_name varchar(50),
-            email varchar(70),
-            password varchar(50),
-            confirm_password varchar(50)
+            first_name VARCHAR(50),
+            last_name VARCHAR(50),
+            email VARCHAR(70),
+            password VARCHAR(50),
+            confirm_password VARCHAR(50)
         );
     '''
     cursor.execute(create_register_table)
 
+    # Insert data into the Register table
     insert_into_register = '''
-        Insert into Register(first_name, last_name, email, password, confirm_password) 
+        INSERT INTO Register(first_name, last_name, email, password, confirm_password) 
         VALUES(%s, %s, %s, %s, %s);
     '''
     cursor.execute(insert_into_register, (first_name, last_name, email, password, confirm_password))
     db.connection.commit()
+
     return jsonify(data), 200
 
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
+@app.route('/api/get_data/<int:id>', methods=['GET'])
+def get_data(id):
+    # Fetch data from the database using the ID
+    cursor.execute('SELECT * FROM Register WHERE id = %s', (id,))
+    user_data = cursor.fetchone()
 
+    if user_data:
+        # Return the user data as JSON
+        return jsonify({
+            'id': user_data[0],
+            'first_name': user_data[1],
+            'last_name': user_data[2],
+            'email': user_data[3],
+            'password': user_data[4],  
+            'confirm_password': user_data[5]
+        }), 200
+    else:
+        # If the ID is not found
+        return jsonify({'message': 'User not found'}), 404
 
-app.run()
+# Run the Flask app
+if __name__ == '__main__':
+    app.run()
+
+# Close the database connection after the server stops
 db.connection.close()
-
