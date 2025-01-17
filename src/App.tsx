687646c7bf9eb38eff0ca570a0677fe5cd3da6c0
praniil <pparajuli15@gmail.com>
2025-01-17 20:35:09 +0545

    import in app.tsx corrected

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Home from './Components/Home';
 import Login from './Components/Login';
 
 import {
-  BrowserRouter as Router,
   Route,
-  Link,
   BrowserRouter,
   Routes,
 } from "react-router-dom";
@@ -21,7 +19,6 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='register' element= {<Register/>}/>
           <Route path='login' element= {<Login/>}/>
-          <Route path = 'landing' element = {<Landing/>}/>
         </Routes>
       </BrowserRouter>
     </>
